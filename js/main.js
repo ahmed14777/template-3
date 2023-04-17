b2428000f0708ab7da8288320167a9f680d1094d
ahmed14777 <ah9621777@gmail.com>
2023-04-17 04:50:16 +0200

    block js toggle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-let otherLinks = document.querySelector(".mega-container");
-let megaMenu = document.getElementById("mega");
-console.log(megaMenu);
-otherLinks.onclick = function () {
-    megaMenu.classList.toggle("mega-flex");
-};
+// let otherLinks = document.querySelector(".mega-container");
+// let megaMenu = document.getElementById("mega");
+// console.log(megaMenu);
+// otherLinks.onclick = function () {
+//     megaMenu.classList.toggle("mega-flex");
+// };
