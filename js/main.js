ee414727d26aff42a2873771831cd26862795afe
ahmed14777 <ah9621777@gmail.com>
2023-05-01 01:55:36 +0200

    add observer

--- js/main.js
+++ js/main.js
@@ -4,3 +4,24 @@ console.log(megaMenu);
 otherLinks.onclick = function () {
     megaMenu.classList.toggle("mega-flex");
 };
+
+//  start animation for website
+const ele = document.querySelectorAll("body > *");
+ele.forEach((ele) => {
+    ele.classList.add("show");
+});
+console.log(ele);
+const options = {
+    root: null,
+    rootMargin: "-140px 0px -100px 0px",
+};
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((e) => {
+        e.target.classList.toggle("show", e.isIntersecting);
+    });
+}, options);
+
+ele.forEach((e) => {
+    observer.observe(e);
+});
+//end  animation for website
